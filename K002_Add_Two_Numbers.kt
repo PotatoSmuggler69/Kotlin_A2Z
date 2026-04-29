2b3a8a58f14389d530b447a03e5d5d2c140005be
thenavneety <navneetyadav2581@gmail.com>
2026-04-30 01:12:06 +0530

    random

--- K002_Add_Two_Numbers.kt
+++ K002_Add_Two_Numbers.kt
@@ -1,13 +1,3 @@
 fun main(){
     print("Enter the number 1: ")
-    val a : Int? = readLine()?.toInt()
-    print("Enter the numebr 2 : ")
-    val b: Int? = readLine()?.toInt()
-
-    if(a!=null && b!=null)   sum(a,b)
-}
-
-fun sum(a: Int?,b: Int?){
-    if(a!=null && b!=null) print("The sum is ${a+b}")
-    else print("Enter correct data")
 }
